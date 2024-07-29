ec6d359b0ee638ebffe22eed2f3855382d294287
lsh1202 <vic071202@gmail.com>
2024-07-29 10:24:57 +0900

    :sparkles: :: QvickButton에 destination 추가

--- Sources/QDS/Component/Button/QvickButton.swift
+++ Sources/QDS/Component/Button/QvickButton.swift
@@ -9,13 +9,15 @@ import SwiftUI
 
 struct QvickButton: View {
     let action: (() -> ())?
+    let destination: (() -> AnyView)?
     let text: String
     let background: Color
         
-    public init(action: (() -> ())? = nil, text: String, background: Color = .blue) {
+    public init(action: (() -> ())? = nil, destination: (() -> AnyView)? = nil, text: String, background: Color = .blue) {
         self.text = text
         self.background = background
         self.action = action
+        self.destination = destination
     }
     
     var body: some View {
